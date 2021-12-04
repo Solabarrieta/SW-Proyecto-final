3594ebef35b3bc7aa6a725fea05ca66a7c8f04d2
Oier <oieruni22@gmail.com>
2021-12-04 21:13:58 +0100

    Quitando el codigo del servicio soap debido a que no funciona

--- php/ClientVerifyEnrollment.php
+++ php/ClientVerifyEnrollment.php
@@ -1,7 +1,9 @@
 <?php
 if (isset($correo)) {
     $soapClient = new SoapClient('http://ehusw.es/jav/ServiciosWeb/comprobarmatricula.php?wsdl');
+    die("error del client verify");
     $response = $soapClient->comprobar($correo);
+    die($response);
     $valido = False;
     if ($response == 'SI') {
         $valido = True;
